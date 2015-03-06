16982b153ac50970e1920371c38b548ea831fedb
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-03-06 23:30:05 +0000

    updated launch scripts

--- sources/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
+++ sources/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
@@ -1,1 +1,1 @@
-java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar jars/com.nextbreakpoint.nextfractal.build-7.0.0-jar-with-depencencies.jar
+java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar com.nextbreakpoint.nextfractal.runtime-7.0.0.jar
--- sources/com.nextbreakpoint.nextfractal.build/resources/exe/win32/NextFractal.bat
+++ sources/com.nextbreakpoint.nextfractal.build/resources/exe/win32/NextFractal.bat
@@ -1,1 +1,1 @@
-start javaw -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar jars/com.nextbreakpoint.nextfractal.build-7.0.0-jar-with-depencencies.jar
+start javaw -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar com.nextbreakpoint.nextfractal.runtime-7.0.0.jar
