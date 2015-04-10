ab57f2722bd1cbd6af734b91d75ee6cbcf1e7e7e
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-04-10 19:54:58 +0100

    Updated copyright notice

--- sources/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/RendererView.java
+++ sources/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/RendererView.java
@@ -29,30 +29,6 @@ import com.nextbreakpoint.nextfractal.core.utils.Integer4D;
 import com.nextbreakpoint.nextfractal.mandelbrot.MandelbrotView;
 import com.nextbreakpoint.nextfractal.mandelbrot.core.Number;
 
-/*
- * JAME 6.2
- * http://jame.sourceforge.net
- *
- * Copyright 2001, 2015 Andrea Medeghini
- *
- * This file is part of JAME.
- *
- * JAME is an application for creating fractals and other graphics artifacts.
- *
- * JAME is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * JAME is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with JAME.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 public class RendererView {
 	private Double4D traslation;
 	private Double4D rotation;
--- sources/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosDynamic.java
+++ sources/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosDynamic.java
@@ -24,30 +24,6 @@
  */
 package com.nextbreakpoint.nextfractal.mandelbrot.renderer.xaos;
 
-/*
- * JAME 6.2
- * http://jame.sourceforge.net
- *
- * Copyright 2001, 2015 Andrea Medeghini
- *
- * This file is part of JAME.
- *
- * JAME is an application for creating fractals and other graphics artifacts.
- *
- * JAME is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * JAME is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with JAME.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 class XaosDynamic {
 	int[] delta;
 	XaosPrice[] oldBest;
