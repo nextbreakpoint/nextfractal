dbab236c687e9c928082edf43a8f84d7cce6ec47
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-04-22 00:49:50 +0100

    Fixed palette index

--- maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/core/Palette.java
+++ maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/core/Palette.java
@@ -41,7 +41,7 @@ public class Palette {
 
 	public float[] get(double n) {
 		if (table != null) {
-			return table[((int)Math.rint(n - 1)) % table.length];
+			return table[((int)Math.rint(n)) % table.length];
 		}
 		return DEFAULT;
 	}
