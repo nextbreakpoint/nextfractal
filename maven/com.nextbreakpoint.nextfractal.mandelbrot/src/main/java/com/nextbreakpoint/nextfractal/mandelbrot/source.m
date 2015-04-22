f909ef7f59918f41cb5ec7a99dbc045d9b23270d
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-04-22 20:43:47 +0100

    Updated initial source

--- maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/source.m
+++ maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/source.m
@@ -1,24 +1,15 @@
 fractal {
-	orbit [<-2.5,-1.5>,<0.5,1.5>] [z,n] {
-		begin {
-			z = x;
-		}
-		loop [0, 200] (mod2(z) > 4) {
-			z = z * z + w;
+	orbit [<-2.5,-1.5>,<0.5,1.5>] [x,n] {
+		loop [0, 200] (mod2(x) > 4) {
+			x = x * x + w;
 		}
 	}
 	color [(1,0,0,0)] {
 		rule (re(n) = 0) [1.0] {
-			1, // alpha
-			0, // red
-			0, // green
-			0  // blue
+			1,0,0,0
 		}
 		rule (re(n) > 0) [1.0] {
-			1, // alpha
-			1, // red
-			1, // green
-			1  // blue
+			1,1,1,1
 		}
 	}
 }
