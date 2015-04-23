4dd08e74f5abe77cec6ac64d91e6cd1336706786
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-04-23 21:50:05 +0100

    Updated jar version in boot script

--- maven/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
+++ maven/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd NextFractal
-java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar com.nextbreakpoint.nextfractal.runtime-1.0.0.jar
+java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar com.nextbreakpoint.nextfractal.runtime-1.0.1.jar
