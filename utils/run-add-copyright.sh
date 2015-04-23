a50ecc4f98a740cf38964068b2361e0d9156676b
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-04-23 21:12:16 +0100

    Updated notice

--- utils/run-add-copyright.sh
+++ utils/run-add-copyright.sh
@@ -2,4 +2,4 @@ export ROOT=`pwd`/..
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven
 
-java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright3.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright-xaos.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
--- utils/run-change-copyright.sh
+++ utils/run-change-copyright.sh
@@ -2,5 +2,5 @@ export ROOT=`pwd`/..
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven
 
-java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright3.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright-xaos.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
 
