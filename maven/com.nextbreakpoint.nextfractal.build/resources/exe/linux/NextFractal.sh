c300b9f696b1a16fad7a76e7574752ba0e0273c0
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-06-05 23:37:54 +0100

    Removed property mandelbrot.browser.defult

--- maven/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
+++ maven/com.nextbreakpoint.nextfractal.build/resources/exe/linux/NextFractal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd NextFractal
-java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -Dmandelbrot.browser.default=#[user.dir]/examples -jar com.nextbreakpoint.nextfractal.runtime-1.0.5.jar
+java -Xmx1024m -Djava.util.logging.config=com.nextbreakpoint.nextfractal.runtime.LogConfig -jar com.nextbreakpoint.nextfractal.runtime-1.0.5.jar
