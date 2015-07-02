9276e1101b3ebeabbe55cd184e329d17fa3478a7
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-07-02 20:27:14 +0100

    New build scripts

--- utils/run-add-copyright.sh
+++ utils/run-add-copyright.sh
@@ -1,5 +1,15 @@
 export ROOT=`pwd`/..
 
-java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.core/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.contextfree/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.runtime/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.server/src/main/java
+
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.core/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.contextfree/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.runtime/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.server/src/test/java
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.AddCopyright copyright-xaos.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
--- utils/run-change-copyright.sh
+++ utils/run-change-copyright.sh
@@ -1,6 +1,16 @@
 export ROOT=`pwd`/..
 
-java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.core/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.contextfree/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.runtime/src/main/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.server/src/main/java
+
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.core/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.contextfree/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.runtime/src/test/java
+java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright.txt $ROOT/maven/com.nextbreakpoint.nextfractal.server/src/test/java
 
 java -classpath FileUtility.jar net.sf.jame.fileutility.ChangeCopyright copyright-xaos.txt $ROOT/maven/com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/renderer/xaos/XaosRenderer.java
 
