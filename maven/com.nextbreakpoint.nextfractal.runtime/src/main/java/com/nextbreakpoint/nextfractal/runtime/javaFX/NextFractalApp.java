48e058c673cb2b604f284724caa3f0a037b8c8bf
Andrea Medeghini <andrea@nextbreakpoint.com>
2015-08-21 08:46:35 +0100

    Added app version, copyright and license in log output

--- maven/com.nextbreakpoint.nextfractal.runtime/src/main/java/com/nextbreakpoint/nextfractal/runtime/javaFX/NextFractalApp.java
+++ maven/com.nextbreakpoint.nextfractal.runtime/src/main/java/com/nextbreakpoint/nextfractal/runtime/javaFX/NextFractalApp.java
@@ -102,6 +102,21 @@ public class NextFractalApp extends Application {
 			dialog.showAndWait();
 		}
 		
+		logger.info("\n\nNextFratal 1.2\n\n" +
+			 "https://github.com/nextbreakpoint/nextfractal\n\n" +
+			 "Copyright 2015 Andrea Medeghini\n\n" +
+			 "NextFractal is an application for creating fractals and other graphics artifacts.\n\n" +
+			 "NextFractal is free software: you can redistribute it and/or modify\n" +
+			 "it under the terms of the GNU General Public License as published by\n" +
+			 "the Free Software Foundation, either version 3 of the License, or\n" +
+			 "(at your option) any later version.\n\n" +
+			 "NextFractal is distributed in the hope that it will be useful,\n" +
+			 "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+			 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+			 "GNU General Public License for more details.\n\n" +
+			 "You should have received a copy of the GNU General Public License\n" +
+			 "along with NextFractal.  If not, see <http://www.gnu.org/licenses/>.\n\n");
+
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		logger.info("DPI = " + Screen.getPrimary().getDpi());
 		logger.info("Screen Size = (" + primaryScreenBounds.getWidth() + "," + primaryScreenBounds.getHeight() + ")");
