b0aef7d196f97fd2a2247caf0e166979eb313d6b
Andrea Medeghini <andrea@nextbreakpoint.com>
2016-09-10 21:25:08 +0100

    Partial implementation of CFDG rendering

--- com.nextbreakpoint.nextfractal.contextfree/src/test/java/com/nextbreakpoint/nextfractal/contextfree/test/V3RenderTest.java
+++ com.nextbreakpoint.nextfractal.contextfree/src/test/java/com/nextbreakpoint/nextfractal/contextfree/test/V3RenderTest.java
@@ -127,85 +127,8 @@ public class V3RenderTest extends AbstractBaseTest {
 	}
 
 	private class TestCanvas extends SimpleCanvas {
-		private AffineTransform currTransform = new AffineTransform();
-		private BufferedImage image;
-		private Graphics2D g2d;
-
 		private TestCanvas(int width, int height) {
 			super(width, height);
-			image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		}
-
-		public void primitive(int shapeType, double[] color, AffineTransform transform) {
-			g2d.setColor(new Color((float)color[0], (float)color[1], (float)color[2], (float)color[3]));
-			AffineTransform oldTransform = g2d.getTransform();
-			AffineTransform t = new AffineTransform(currTransform);
-			t.concatenate(transform);
-			g2d.setTransform(t);
-			PrimShape primShape = PrimShape.getShapeMap().get(shapeType);
-			if (primShape != null) {
-				g2d.fill(primShape.getPath());
-			} else {
-				throw new RuntimeException("Unexpected shape " + shapeType);
-			}
-			g2d.setTransform(oldTransform);
-		}
-
-		public void path(double[] color, AffineTransform transform, CommandInfo attr) {
-			g2d.setColor(new Color((float)color[0], (float)color[1], (float)color[2], (float)color[3]));
-			AffineTransform oldTransform = g2d.getTransform();
-			AffineTransform t = new AffineTransform(currTransform);
-			t.concatenate(transform);
-			g2d.setTransform(t);
-			g2d.setStroke(new BasicStroke((float)attr.getStrokeWidth(), mapToCap(attr.getFlags()), mapToJoin(attr.getFlags()), (float)attr.getMiterLimit()));
-			ExtendedGeneralPath path = attr.getPathStorage().getGeneralPath();
-			if ((attr.getFlags() & FlagType.CF_FILL.getMask()) != 0) {
-				g2d.fill(path);
-			} else {
-				g2d.draw(path);
-			}
-			g2d.setTransform(oldTransform);
-		}
-
-		private int mapToJoin(int flags) {
-			if ((flags & FlagType.CF_MITER_JOIN.getMask()) != 0) {
-				return BasicStroke.JOIN_MITER;
-			} else if ((flags & FlagType.CF_ROUND_JOIN.getMask()) != 0) {
-				return BasicStroke.JOIN_ROUND;
-			} else if ((flags & FlagType.CF_BEVEL_JOIN.getMask()) != 0) {
-				return BasicStroke.JOIN_BEVEL;
-			} else {
-				throw new RuntimeException("Invalid flags " + flags);
-			}
-		}
-
-		private int mapToCap(int flags) {
-			if ((flags & FlagType.CF_BUTT_CAP.getMask()) != 0) {
-				return BasicStroke.CAP_BUTT;
-			} else if ((flags & FlagType.CF_ROUND_CAP.getMask()) != 0) {
-				return BasicStroke.CAP_ROUND;
-			} else if ((flags & FlagType.CF_SQUARE_CAP.getMask()) != 0) {
-				return BasicStroke.CAP_SQUARE;
-			} else {
-				throw new RuntimeException("Invalid flags " + flags);
-			}
-		}
-
-		public void start(boolean first, double[] backgroundColor, int currWidth, int currHeight) {
-			g2d = image.createGraphics();
-			g2d.setColor(new Color((float)backgroundColor[0], (float)backgroundColor[1], (float)backgroundColor[2], (float)backgroundColor[3]));
-			g2d.fillRect(0, 0, getWidth(), getHeight());
-			currTransform = AffineTransform.getTranslateInstance(0, getHeight());
-			currTransform.scale(1, -1);
-			currTransform.translate(-(currWidth - getWidth()) / 2, -(currHeight - getHeight()) / 2);
-		}
-
-		public void end() {
-			g2d.dispose();
-		}
-
-		public BufferedImage getImage() {
-			return image;
 		}
 	}
 }
