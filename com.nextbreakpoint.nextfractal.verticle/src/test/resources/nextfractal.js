987fa96916f1c6e458db87a7eca06d79035cabc0
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-05-19 17:29:37 +0100

    Added field for url

--- com.nextbreakpoint.nextfractal.verticle/src/test/resources/nextfractal.js
+++ com.nextbreakpoint.nextfractal.verticle/src/test/resources/nextfractal.js
@@ -2,10 +2,14 @@ $(function() {
 	var script = "fractal {\norbit [-2.0 - 2.0i,+2.0 + 2.0i] [x,n] {\nloop [0, 200] (mod2(x) > 40) {\nx = x * x + w;\n}\n}\ncolor [#FF000000] {\npalette gradient {\n[#FFFFFFFF > #FF000000, 100];\n[#FF000000 > #FFFFFFFF, 100];\n}\ninit {\nm = 100 * (1 + sin(mod(x) * 0.2 / pi));\n}\nrule (n > 0) [1] {\ngradient[m - 1]\n}\n}\n}\n";
 	var metadata = "{\"translation\":{\"x\":0.0,\"y\":0.0,\"z\":1.0,\"w\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":0.0},\"scale\":{\"x\":1.0,\"y\":1.0,\"z\":1.0,\"w\":1.0},\"point\":{\"x\":0.0,\"y\":0.0},\"julia\":false,\"options\":{\"showPreview\":false,\"showTraps\":false,\"showOrbit\":false,\"showPoint\":false,\"previewOrigin\":{\"x\":0.0,\"y\":0.0},\"previewSize\":{\"x\":0.25,\"y\":0.25}}}";
 
+	$("#script").val(script);
+	$("#metadata").val(metadata);
+	$("#uuid").val("00000000-0000-0000-0000-000000000000");
+	$("#url").val("http://localhost:8080/fractals");
+
     var map = L.map('canvas').setView([0, 0], 2);
 
-//    var url = "http://localhost:8080/fractals";
-    var url = "https://dg02mphys2.execute-api.eu-west-1.amazonaws.com/live";
+    var url = $("#url").val();
 
     var layer = L.tileLayer(url + '/00000000-0000-0000-0000-000000000000/{z}/{x}/{y}.png', {
         attribution: '&copy; Andrea Medeghini',
@@ -19,10 +23,6 @@ $(function() {
 /*    L.marker([0, 0]).addTo(map)
         .bindPopup('A pretty fractal')
         .openPopup();*/
-
-	$("#script").val(script);
-	$("#metadata").val(metadata);
-	$("#uuid").val("00000000-0000-0000-0000-000000000000");
 
     $("#formCreate").submit(function(event) {
         event.preventDefault();
@@ -54,6 +54,7 @@ $(function() {
 	$("#formRender").submit(function(event) {
         event.preventDefault();
 
+        var url = $("#url").val();
         var uuid = $("#uuid").val();
 
         layer.setUrl(url + '/' + uuid + '/{z}/{x}/{y}.png');
