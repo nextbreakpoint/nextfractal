ed59c9b7beaaa1687f0cd66c2bd96c08300bee76
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-02-13 08:32:43 +0000

    Rename output file

--- utils/grammars.sh
+++ utils/grammars.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sh grammar.sh $(pwd)/../com.nextbreakpoint.nextfractal.mandelbrot/src/main/java/com/nextbreakpoint/nextfractal/mandelbrot/grammar/Mandelbrot.g4 >Mandelbrot.txt
-sh grammar.sh $(pwd)/../com.nextbreakpoint.nextfractal.contextfree/src/main/java/com/nextbreakpoint/nextfractal/contextfree/grammar/CFDG.g4 >CFDG4.txt
+sh grammar.sh $(pwd)/../com.nextbreakpoint.nextfractal.contextfree/src/main/java/com/nextbreakpoint/nextfractal/contextfree/grammar/CFDG.g4 >CFDG.txt
